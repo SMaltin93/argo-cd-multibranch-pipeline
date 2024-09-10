643c52d6f810ff7dbefdeb11e325238cbbe6c185
sam <sam.maltin@gmail.com>
2024-09-10 11:12:11 +0200

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "*******SThis is a newestttttt"
+    return "*******Now it is newest **************************''"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
