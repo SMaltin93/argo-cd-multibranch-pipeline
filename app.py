f432a550efaa602514a1818efce4dc84768686f9
sam <sam.maltin@gmail.com>
2024-09-10 16:55:08 +0200

    forces the pod to restart when the deployment is synced

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "******* restart when sync *******"
+    return "******* forces the pod to restart when the deployment is synced *******"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
