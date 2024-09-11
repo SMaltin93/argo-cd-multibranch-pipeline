ac79f2e0384e8db9e8cc8047af0b153f033b69e5
sam <sam.maltin@gmail.com>
2024-09-11 10:00:15 +0200

    new test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "******* kustomize *******"
+    return "******* test *******"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
