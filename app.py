5d6080d66211c70984f1f69174f49d2138cf3d56
sam <sam.maltin@gmail.com>
2024-09-11 09:38:49 +0200

    uppdate

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "******* compose updated *******"
+    return "******* uppdate *******"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
